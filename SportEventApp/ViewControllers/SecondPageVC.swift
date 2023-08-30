eb4adcf8cb196c1e3589771d4990e9b79753708d
MelnykovMykyta <melnikovnikita@ukr.net>
2023-08-31 02:34:58 +0300

    Melnykov_HM_05

---
+++ SportEventApp/ViewControllers/SecondPageVC.swift
@@ -0,0 +1,17 @@
+//
+//  SecondPageVC.swift
+//  SportEventApp
+//
+//  Created by Nikita Melnikov on 31.08.2023.
+//
+
+import Foundation
+import UIKit
+
+class SecondPageVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
