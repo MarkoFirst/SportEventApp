1bb8d0358dc3c82b8e3fa71cda9079e8405f3f05
MelnykovMykyta <melnikovnikita@ukr.net>
2023-09-03 00:11:33 +0300

    elements sign up

---
+++ SportEventApp/ViewControllers/SignUpVC.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpVC.swift
+//  SportEventApp
+//
+//  Created by Nikita Melnikov on 02.09.2023.
+//
+
+import Foundation
+import UIKit
+
+class SignUpVC: UIViewController, UITextFieldDelegate {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        nameTF.resignFirstResponder()
+//    }
+    
+}
